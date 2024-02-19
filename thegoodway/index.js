6e4da524a8727cd574ff3d4e0d067a49ba3bfff6
Nikhil Raj <menikhilraj05@gmail.com>
2024-02-19 12:59:20 +0530

    Used app.use for the middlewares

--- thegoodway/index.js
+++ thegoodway/index.js
@@ -2,38 +2,35 @@ const express = require("express");
 
 const app = express();
 
-function userMiddleware(req,res,next){
-    
+function userMiddleware(req, res, next) {
+
     const username = req.headers.username;
     const password = req.headers.password;
 
-    if(username == "nikhil"  && password == "pass"){
+    if (username == "nikhil" && password == "pass") {
         next();
-    }else{
+    } else {
         res.status(400).json({
-            msg : "Invalid credentials"
+            msg: "Invalid credentials"
         })
     }
 }
 
-function kidneyMiddleware(req,res,next){
+function kidneyMiddleware(req, res, next) {
     const kidneyId = req.query.kidneyId;
-    if(kidneyId == 1 || kidneyId == 2){
+    if (kidneyId == 1 || kidneyId == 2) {
         next();
-    }else{
+    } else {
         res.status(400).json({
-            msg : "your kidney is not doing good"
+            msg: "your kidney is not doing good"
         })
     }
-
-
 }
 
+app.use(userMiddleware, kidneyMiddleware);
 
-app.get("/health-checkup",userMiddleware , kidneyMiddleware, function(req,res){
 
-   
-
+app.get("/health-checkup", function (req, res) {
     res.send("you are healthy");
 })
 
