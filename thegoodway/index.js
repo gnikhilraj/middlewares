7afb4e8cb2b5ed139f514e55bc8bc276f1d2ac26
Nikhil Raj <menikhilraj05@gmail.com>
2024-02-19 12:49:44 +0530

    commited the goodway code for middlewares

---
+++ thegoodway/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+
+function userMiddleware(req,res,next){
+    
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    if(username == "nikhil"  && password == "pass"){
+        next();
+    }else{
+        res.status(400).json({
+            msg : "Invalid credentials"
+        })
+    }
+}
+
+function kidneyMiddleware(req,res,next){
+    const kidneyId = req.query.kidneyId;
+    if(kidneyId == 1 || kidneyId == 2){
+        next();
+    }else{
+        res.status(400).json({
+            msg : "your kidney is not doing good"
+        })
+    }
+
+
+}
+
+
+app.get("/health-checkup",userMiddleware , kidneyMiddleware, function(req,res){
+
+   
+
+    res.send("you are healthy");
+})
+
+
+
+app.listen(3000);
